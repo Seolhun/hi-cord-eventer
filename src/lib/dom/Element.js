0c3ef81407afa9177461260ccb78ba1a6d770652
Seolhun <shun10114@gmail.com>
2018-04-13 19:33:27 +0900

    [API] Dom API(Element, ElementItem) is Updated

--- src/lib/dom/Element.js
+++ src/lib/dom/Element.js
@@ -1,38 +1,48 @@
+import classnames from 'classnames';
+
 export default class Element {
   constructor({ tag, attributes, children, on }) {
-    this.tag = tag
-    this.attributes = attributes
-    this.children = children
-    this.on = on
+    this.tag = tag;
+    this.attributes = attributes;
+    this.children = children;
+    this.on = on;
   }
 
   render() {
+    // Tag
     if (!this.tag || typeof this.tag !== 'string') {
       throw new Error('Requirement tag param in Element.js');
     }
     const created_element = document.createElement(this.tag);
 
+    // Attributes
     if(this.attributes && typeof this.attributes !== 'object') {
       throw new Error('The Element attributes have to be object type');
     }
     if (this.attributes) {
       for (let i in this.attributes) {
-        created_element[i] = this.attributes[i];
+        if(i === 'className') {
+          created_element[i] = classnames(this.attributes[i]);
+        } else {
+          created_element[i] = this.attributes[i];
+        }
       }
     }
 
-    if(this.children && typeof this.children !== 'object') {
-      throw new Error('The Element children have to be object type');
+    // Children
+    if(this.children && !Array.isArray(this.children)) {
+      throw new Error('The Element children have to be Array type');
     }
     if (this.children) {
-      for (let child in this.children) {
-        if(!this.children[child] instanceof Element) {
+      for (let child of this.children) {
+        if(!child instanceof Element) {
           throw new Error('The Element children have to be build by Element.js');
         }
-        created_element.appendChild(this.children[child].render())
+        created_element.appendChild(child.render())
       }
     }
 
+    // On(Event)
     if (this.on && typeof this.on.event !== 'string') {
       if (this.on.event && typeof this.on.event !== 'string') {
         throw new Error('The Element on.event have to be string type');
---
+++ src/lib/dom/ElementItem.js
@@ -0,0 +1,7 @@
+export default class ElementItem {
+  constructor({ value, tagName, className }) {
+    this.value = value;
+    this.tagName = tagName;
+    this.className = className;
+  }
+}
