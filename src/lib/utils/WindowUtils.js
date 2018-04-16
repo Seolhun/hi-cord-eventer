5454145539388042291807ad28db0cace533da6c
Seolhun <shun10114@gmail.com>
2018-04-16 18:58:40 +0900

    [API] Updated Mobile and Action Utils

---
+++ src/lib/utils/WindowUtils.js
@@ -0,0 +1,17 @@
+class WindowUtils {
+  // Necessary `babel-preset-stage-0`
+  static _diff_value = 16;
+  static _mobile_size = 768;
+  static is_mobile = false;
+
+  static isMobile(window) {
+    let result = false;
+    const client_width = document.body.clientWidth + this._diff_value;
+    if (client_width <= this._mobile_size) {
+      result = true;
+    }
+    return result;
+  }
+}
+
+export default WindowUtils;
