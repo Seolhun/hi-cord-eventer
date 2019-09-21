335b788866bc07dbca675b68600c10b840870463
Seolhun <shun10114@gmail.com>
2019-09-21 17:59:39 +0900

    Update examples for packages

--- examples/rollup.config.js
+++ examples/rollup.config.js
@@ -1,12 +1,19 @@
 import serve from "rollup-plugin-serve";
+import babel from "rollup-plugin-babel";
+
+import pkg from "../package.json";
+
+const externals = Object.keys(pkg.dependencies);
 
 export default {
+  external: [...externals],
   input: "index.js",
   output: {
-    file: "dest.js",
+    file: "dist/banner.js",
     format: "cjs",
   },
   plugins: [
+    babel(),
     serve({
       open: true,
       openPage: "/index.html",
