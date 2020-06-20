4aecd523254a394542736e46687a360178167807
Seolhun <shun10114@gmail.com>
2020-06-20 11:04:38 +0900

    update tsconfig with declartion custom

---
+++ packages/banners/@types/custom.d.ts
@@ -0,0 +1,19 @@
+declare module '*.ico' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.svg' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.scss' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.png' {
+  const content: string;
+  export default content;
+}
