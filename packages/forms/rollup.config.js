3cdab19274792351d77a1ea009e3403b12823090
Seolhun <shun10114@gmail.com>
2020-06-20 10:58:16 +0900

    update all package configuration from js to ts

--- packages/forms/rollup.config.js
+++ packages/forms/rollup.config.js
@@ -1,60 +1,53 @@
-import autoprefixer from 'autoprefixer';
-import babel from 'rollup-plugin-babel';
-import postcss from 'rollup-plugin-postcss';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
+import resolve from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
 
-import pkg from './package.json';
+import pkg from '../../package.json';
 
-const isProduction = process.env.NODE_ENV === 'production';
+let externals = [];
+if (pkg.dependencies) {
+  externals = Object.keys(pkg.dependencies);
+}
 
-const externals = Object.keys(pkg.dependencies);
-
-export default (async () => ({
-  external: [
-    ...externals,
-    'path',
-    'fs',
-    'resolve',
-    'rollup-pluginutils',
-    'typescript',
-  ],
-  input: 'src/index.js',
+export default {
+  input: './src/index.ts',
   output: [
     {
       format: 'cjs',
-      file: pkg.main,
-      sourcemap: isProduction,
+      file: 'dist/index.js',
     },
     {
       format: 'es',
-      file: pkg.module,
-      sourcemap: isProduction,
+      file: 'dist/index.esm.js',
+    },
+    {
+      format: 'cjs',
+      file: 'dist/index.min.js',
+      plugins: [terser()],
+    },
+    {
+      format: 'es',
+      file: 'dist/index.esm.min.js',
+      plugins: [terser()],
     },
   ],
-
+  external: [...externals],
   plugins: [
     resolve({
-      dedupe: ['react', 'react-dom'],
       mainFields: ['module', 'main'],
       extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
-    }),
-    typescript({
-      tsconfig: 'tsconfig.json',
     }),
     commonjs({
       include: /node_modules/,
     }),
+    typescript({
+      tsconfig: 'tsconfig.json',
+    }),
     babel({
+      babelHelpers: 'bundled',
       exclude: /node_modules/,
     }),
-    postcss({
-      extract: true,
-      plugins: [autoprefixer],
-      modules: true,
-    }),
-    isProduction && terser(),
   ],
-}))();
+};
