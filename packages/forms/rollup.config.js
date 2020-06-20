7b03e5cb3602e93e03d9e4230fd0a6790ac100ee
Seolhun <shun10114@gmail.com>
2020-06-21 06:49:46 +0900

    update rollup styles prefixer

--- packages/forms/rollup.config.js
+++ packages/forms/rollup.config.js
@@ -3,6 +3,8 @@ import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
 import resolve from '@rollup/plugin-node-resolve';
 import postcss from 'rollup-plugin-postcss';
+import autoprefixer from 'autoprefixer';
+import postcssFlexboxfixer from 'postcss-flexboxfixer';
 import { terser } from 'rollup-plugin-terser';
 
 import pkg from '../../package.json';
